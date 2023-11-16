6e926da61b4350ba165fe417f7d6216607a7a6b2
Odinaka nnamani <Robertpaschal>
2023-11-16 03:12:39 -0800

    pchar

--- execute.c
+++ execute.c
@@ -37,6 +37,8 @@ void execute_command(char *line, unsigned int line_number, stack_t **stack)
 		mul(stack, line_number);
 	else if (strcmp(opcode, "mod") == 0)
 		mod(stack, line_number);
+	else if (strcmp(opcode, "pchar") == 0)
+		pchar(stack, line_number);
 	else
 	{
 		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
---
+++ pchar.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * pchar - Prints the char at the top of the stack
+ * @stack: Pointer to the top of the satck
+ * @line_number: Line number in the script
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+	if (!stack || !*stack)
+	{
+		fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n < 0 || (*stack)->n > 127)
+	{
+		fprintf(stderr, "L%d: can't pchar, value out of range\n",line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	putchar((*stack)->n);
+	putchar('\n');
+}
